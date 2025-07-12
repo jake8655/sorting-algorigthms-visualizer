298219b024b411de4a7961df6320344490dc7156
Jake <dominik8655@gmail.com>
2025-07-12 20:53:33 +0200

    refactor(navigation): get link active status with recommended method by next-intl docs

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,6 +2,7 @@
 
 import { motion } from "motion/react";
 import Image from "next/image";
+import { useSelectedLayoutSegment } from "next/navigation";
 import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 import { Link, usePathname } from "@/i18n/navigation";
@@ -75,7 +76,6 @@ export default function Navbar() {
 function NavLink({
 	name,
 	href,
-	isActive,
 	className,
 }: {
 	name: "home" | "playground";
@@ -85,8 +85,13 @@ function NavLink({
 }) {
 	const t = useTranslations("navigation");
 
+	const selectedLayoutSegment = useSelectedLayoutSegment();
+	const pathname = selectedLayoutSegment ? `/${selectedLayoutSegment}` : "/";
+	const isActive = pathname === href;
+
 	return (
 		<Link
+			aria-current={isActive ? "page" : undefined}
 			href={href}
 			className={cn(
 				"relative px-1 py-2 font-medium text-[oklch(0.984_0.003_247.858)] transition-all",
